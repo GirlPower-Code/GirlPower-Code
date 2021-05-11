a3b3c074a9983a144e9cccac557c9309819b2846
Cacau Ribeiro <ribeirocarolina767@gmail.com>
2021-05-11 15:12:41 -0300

    Primeira versão
    Versão 1.0

---
+++ listaDesejos/src/main/java/com/listaDesejos/listaDesejos/entity/Client.java
@@ -0,0 +1,98 @@
+package com.listaDesejos.listaDesejos.entity;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="client")
+public class Client implements Serializable {
+
+    private static final long serialVersionUID =1l;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long ID;
+
+    @Column
+    @NotNull
+    private String name;
+
+    @Column
+    @NotNull
+    private String cpf;
+
+    @Column
+    @NotNull
+    private String birthday;
+
+    @Column
+    @NotNull
+    private String number;
+
+    @Column
+    @NotNull
+    private String email;
+
+    @Column
+    @NotNull
+    private String password;
+
+
+    public Long getID() {
+        return ID;
+    }
+
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ listaDesejos/src/main/java/com/listaDesejos/listaDesejos/entity/Product.java
@@ -0,0 +1,62 @@
+package com.listaDesejos.listaDesejos.entity;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="product")
+public class Product implements Serializable {
+
+    private static final long serialVersionUID =1l;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long ID;
+
+    @Column
+    @NotNull
+    private String name;
+
+    @Column
+    @NotNull
+    private double prace;
+
+    @Column
+    @NotNull
+    private String descripcion;
+
+
+    public Long getID() {
+        return ID;
+    }
+
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrace() {
+        return prace;
+    }
+
+    public void setPrace(double prace) {
+        this.prace = prace;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
