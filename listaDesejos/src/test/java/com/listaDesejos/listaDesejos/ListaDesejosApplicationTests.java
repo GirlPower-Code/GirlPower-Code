c959c99467b148d3c070e16bdab5d211a31dce81
Cacau Ribeiro <83369693+Cacaulribeiro@users.noreply.github.com>
2021-05-11 11:59:12 -0300

    Add files via upload

---
+++ listaDesejos/src/test/java/com/listaDesejos/listaDesejos/ListaDesejosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.listaDesejos.listaDesejos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaDesejosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
