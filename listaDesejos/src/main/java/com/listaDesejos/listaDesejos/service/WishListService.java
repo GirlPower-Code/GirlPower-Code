603af815cef709a3b7a762131a1f10f61f948a11
Cacau Ribeiro <ribeirocarolina767@gmail.com>
2021-05-12 14:13:21 -0300

    foi add a wishList_service

---
+++ listaDesejos/src/main/java/com/listaDesejos/listaDesejos/service/WishListService.java
@@ -0,0 +1,31 @@
+package com.listaDesejos.listaDesejos.service;
+
+import com.listaDesejos.listaDesejos.entity.WishList;
+import com.listaDesejos.listaDesejos.repository.WishlistRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WishListService {
+
+    @Autowired
+    private WishlistRepository wishlistRepository;
+
+
+    //add wishlis no banco de dados
+    public WishList adicionarWishList(WishList wishList){
+        return wishlistRepository.save(wishList);
+    }
+
+    //mostrar a lista de desejos do cliente
+    public WishList mostrarWishList(long id){
+        return wishlistRepository.findByID(id);
+    }
+
+
+
+
+
+
+
+}
