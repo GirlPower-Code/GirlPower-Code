9034f9c50826d06c1f0d171a2790e6354c96e6cf
Raissa Vieira <raissa.vieira@lavid.ufpb.br>
2021-05-13 14:52:22 -0300

    criacao dos arquivos de service, repository e controller

---
+++ listaDesejos/src/main/java/com/listaDesejos/listaDesejos/controller/ClientController.java
@@ -0,0 +1,2 @@
+package com.listaDesejos.listaDesejos.controller;public class ClientController {
+}
---
+++ listaDesejos/src/main/java/com/listaDesejos/listaDesejos/repository/ClientRepository.java
@@ -0,0 +1,2 @@
+package com.listaDesejos.listaDesejos.repository;public class ClientRepository {
+}
