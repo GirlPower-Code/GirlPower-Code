268f6d18302294a50cf00264f7e01fc2a24dbea5
Raissa Vieira <raissa.vieira@lavid.ufpb.br>
2021-05-13 14:54:07 -0300

    implementacao do repository de cliente

--- listaDesejos/src/main/java/com/listaDesejos/listaDesejos/repository/ClientRepository.java
+++ listaDesejos/src/main/java/com/listaDesejos/listaDesejos/repository/ClientRepository.java
@@ -1,2 +1,18 @@
-package com.listaDesejos.listaDesejos.repository;public class ClientRepository {
+package com.listaDesejos.listaDesejos.repository;
+
+import com.listaDesejos.listaDesejos.entity.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+
+    public Client save(Client client);
+
+    Optional<Client> findByID(Long id);
+
+    Optional<Client> findByemail(String email);
 }
+
