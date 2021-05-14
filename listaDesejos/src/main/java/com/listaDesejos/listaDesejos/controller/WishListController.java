b37daeeac47bc62179e66bd49eeab2c0110fe05b
Raissa Vieira <raissa.vieira@lavid.ufpb.br>
2021-05-14 00:13:06 -0300

    Finalizacao da rota de buscar produto

--- listaDesejos/src/main/java/com/listaDesejos/listaDesejos/controller/WishListController.java
+++ listaDesejos/src/main/java/com/listaDesejos/listaDesejos/controller/WishListController.java
@@ -6,11 +6,16 @@ import com.listaDesejos.listaDesejos.entity.WishList;
 import com.listaDesejos.listaDesejos.service.ClientService;
 import com.listaDesejos.listaDesejos.service.ProductService;
 import com.listaDesejos.listaDesejos.service.WishListService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @RestController
 @RequestMapping("/wishlist")
 public class WishListController {
@@ -25,17 +30,39 @@ public class WishListController {
     ClientService clientService;
 
     // Pesquisar produto na wishlist
-    @GetMapping("/search/{id}")
-    public boolean findProduct(@PathVariable long id, @RequestParam(value = "name") String name){
-        Optional<Product> searchProduct = productService.exibirProduto(name);
-        Optional<Client> searchClient = clientService.getClientById(id);
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Product> findProduct(@PathVariable long id, @RequestParam(value = "name") String name){
 
-        if( searchProduct.isPresent() && searchClient.isPresent()){
-            WishList wishlist = wishListService.getWishlist(searchClient.get());
+        log.info("Acessando rota de buscar produto pelo nome");
 
-            return wishlist.getProduct().contains(searchProduct.get());
+        try {
+            Optional<Product> searchProduct = productService.exibirProduto(name);
+            Optional<Client> searchClient = clientService.getClientById(id);
+
+            if( searchProduct.isPresent() && searchClient.isPresent()){
+                WishList wishlist = wishListService.getWishlist(searchClient.get());
+                List<Product> products = wishlist.getProduct();
+
+                if(wishlist.getProduct().contains(searchProduct.get())){
+                    Product productFound = new Product();
+
+                    for (Product product : products) {
+                        if(product.getName().equals(name)){
+                            productFound = product;
+
+                            log.info("Produto encontrado e retornado");
+
+                            return new ResponseEntity<>(product, HttpStatus.OK);
+                        }
+                    }
+                }
+            }
+
+            log.info("Produto nao foi encontrado");
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }catch (Exception e){
+            log.error("Falha ao fazer requisicao de buscar por produto");
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-
-        return false;
     }
 }
